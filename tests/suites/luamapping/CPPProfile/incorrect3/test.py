56f3285528387713721336b973527528a812114c
gueckmooh <gueckmooh@pm.me>
2022-04-22 20:02:01 +0200

    Adding test suites

---
+++ tests/suites/luamapping/CPPProfile/incorrect3/test.py
@@ -0,0 +1,22 @@
+from test_suite import TestSuite, assertReturnOk
+
+
+class CPPProfileIncorrect3(TestSuite):
+    def TestBuild(self):
+        with self.sandbox() as s:
+            self.runBS(["build"]).mustBeNOk()
+
+    def TestBuildAndClean(self):
+        with self.sandbox() as s:
+            self.runBS(["build"]).mustBeNOk()
+            self.runBS(["clean"]).mustBeNOk()
+
+    def TestClean(self):
+        with self.sandbox() as s:
+            self.runBS(["clean"]).mustBeNOk()
+
+    def TestBuildCleanRebuild(self):
+        with self.sandbox() as s:
+            self.runBS(["build"]).mustBeNOk()
+            self.runBS(["clean"]).mustBeNOk()
+            self.runBS(["build"]).mustBeNOk()
